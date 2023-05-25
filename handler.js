72488c0817a3038b8397fbb869c59df1d246cfdb
Bob Showalter <showaltb@gmail.com>
2023-05-25 14:03:36 -0400

    Allow errors to be raised

--- handler.js
+++ handler.js
@@ -21,44 +21,43 @@ const processBatch = async () => {
     }
   })
 
-  const list = await pop3.UIDL()
-  for (const [msgNumber, uid] of list) {
-    // handle message that cannot be retrieved for some reason
-    // if (uid === 'GmailId1827c40e8c023dac') {
-    //   await pop3.command('DELE', msgNumber)
-    //   continue
-    // }
-    const [, messageStream] = await pop3.command('RETR', msgNumber)
-    const path = `${DateTime.now().toFormat('yyyy/MM/dd')}/${uid}`
-    console.log('Uploading', path)
-    const upload = new Upload({
-      client: s3,
-      params: {
-        Bucket: process.env.bucket,
-        Key: path,
-        Body: messageStream
-      }
-    })
-    await upload.done()
-  }
-  await pop3.QUIT()
+  try {
+    const list = await pop3.UIDL()
+    for (const [msgNumber, uid] of list) {
+    // // handle message that cannot be retrieved for some reason
+    //   if (uid === 'GmailId1827c40e8c023dac') {
+    //     await pop3.command('DELE', msgNumber)
+    //     continue
+    //   }
+      const [, messageStream] = await pop3.command('RETR', msgNumber)
+      const path = `${DateTime.now().toFormat('yyyy/MM/dd')}/${uid}`
+      console.log('Uploading', path)
+      const upload = new Upload({
+        client: s3,
+        params: {
+          Bucket: process.env.bucket,
+          Key: path,
+          Body: messageStream
+        }
+      })
+      await upload.done()
+    }
 
-  return list.length
+    return list.length
+  } finally {
+    await pop3.QUIT()
+  }
 }
 
 // gmail hands out messages in batches, so process batches until no more
 // messages found
 module.exports.run = async (event, context) => {
-  try {
-    let n
-    while (1) {
-      n = await processBatch()
-      console.log(n, 'messages found')
-      if (n === 0) break
-      console.log('sleeping 2 secs before next check')
-      await new Promise(resolve => setTimeout(resolve, 2000))
-    }
-  } catch (err) {
-    console.log('Error:', err)
+  let n
+  while (1) {
+    n = await processBatch()
+    console.log(n, 'messages found')
+    if (n === 0) break
+    console.log('sleeping 2 secs before next check')
+    await new Promise(resolve => setTimeout(resolve, 2000))
   }
 }
